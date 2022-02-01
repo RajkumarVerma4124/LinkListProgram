22afdf17b499c7edcdc66f35f0b9b40feb034cff
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-01 13:25:07 +0530

    [Raj] Added:- The linklist with addlast and display method

---
+++ LinkListProgram/LinkList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListProgram
+{
+    public class LinkList
+    {
+        public Node head;
+
+        //Creating add last method to add the data in linklist sequentially(UC1)
+        public void AddLast(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.Write("{0} is inserted into linklist", node.data);
+            Console.ReadLine();
+        }
+
+
+       
+        //Displaying the data from linklist nodes(UC1)
+        public void Display()
+        {
+            //Storing the head to temp node
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkList Is Empty");
+                Console.ReadLine();
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data+" ");
+                    temp = temp.next;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- LinkListProgram/Program.cs
+++ LinkListProgram/Program.cs
@@ -10,8 +10,16 @@ namespace LinkListProgram
     {
         public static void Main(string[] args)
         {
+            //Displaying the welcome message
             Console.WriteLine("Welcome To The Linklist Program");
             Console.ReadLine();
+
+            //Creating a simple linklist of 56,30 and 70(UC1)
+            LinkList list = new LinkList();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+            list.Display();
         }
     }
 }
